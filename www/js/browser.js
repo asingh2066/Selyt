de0af7bed19f5beea1eab8759ba0659aa60babf5
bdeasy <bjdeasy@gmail.com>
2016-09-01 21:23:09 -0400

    Editing existing tyles is now enabled

--- www/js/browser.js
+++ www/js/browser.js
@@ -22,19 +22,33 @@ var loadImages = function() {
 	return results;
 };
 
+var editImage = function(image) {
+
+}
+
 var loadBrowsingList = function() {
-	var imagePaths = loadImages();
+	// var imagePaths = loadImages();
 	var container = document.createElement('div');
 	container.id = "wrapper";
 
 	$('body').append(container);
 
 	var html = '';
-	for (var i = 1; i <= imagePaths.length; i++) {
-		html += '<div class="card"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src=" ' + imagePaths[i] + '"></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right"><a href="canvas.html">mode_edit</a></i></span></div>';
+	for (var i = 1; i <= 3; i++) {
+		imageName = "img/browsing/tyle_" + i + ".jpg";
+		html += '<div class="card"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + imageName + '"></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right"><a class="editable">mode_edit</a></i></span></div>';
 	}
 
 	$('#wrapper').append(html);
 };
 
 loadBrowsingList();
+
+$('.editable').click(function() {
+	var image = ($($(this).parent().parent().parent().siblings()[0]).find('.activator').attr('src'));
+	console.log(image);
+	window.canvasImage = image;
+	document.cookie = "canvas_image=" + image + ";path=/";
+
+	window.location.href = "canvas.html?canvas_image=" + image;
+});
